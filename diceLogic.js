b31de625d18d1160e34dc70a334b25c4fdb4f2b5
CitizenThreee <albeisly@gmail.com>
2024-05-02 21:43:43 +1200

    Logic test v1

--- diceLogic.js
+++ diceLogic.js
@@ -1,8 +1,80 @@
+//creates a new dice input element after the last child of the parent dice input div element
 function AddDiceSectionToUi() {
+    diceInputs = document.getElementById("allDice").length;
+
+    document.getElementById("allDice").insertAdjacentHTML("beforeend", 
+    `<div id='diceContainer'>
+        <label for='${"diceType" + diceInputs}' id='diceTypeLabel'>Dice Type:</label>
+        <select name='${"diceType" + diceInputs}' id='${"diceType" + diceInputs}'>
+            <option value='D4'>D4</option>
+            <option value='D6'>D6</option>
+            <option value='D8'>D8</option>
+            <option value='D10'>D10</option>
+            <option value='D12'>D12</option>
+            <option value='D20'>D20</option>
+        </select>
+        <label for='${"diceCount" + diceInputs}' id='diceCountLabel'>Dice Count:</label>
+        <input type='number' value='0' id='${"diceCount" + diceInputs}' name='${"diceCount" + diceInputs}'>
+    </div>`
+);
 }
 
+//Removes the last element child from the dice input div element
 function RemoveDiceSectionFromUi() {
+    document.getElementById("allDice").lastElementChild.remove();
+}
 
+//Adds a dice element for every entry in diceTypes, and assigns the diceTypes display, and diceCount display with their respective values
+function AddDiceElements(diceTypes, diceCounts){
+    let diceHTMLString = "<tr>";
+
+    for(let i = 0; i < diceTypes.length; i++){
+        if(i % 6 == 1){diceHTMLString += "<tr>"}
+
+        diceHTMLString += 
+        `<td>
+            <p class="diceHeader">${diceTypes[i]}</p>
+            <button class="diceButton">${diceCounts[i]}</button>
+        </td>`
+
+        if(i % 6 == 0 || i == diceTypes.length - 1){diceHTMLString += "</tr>"}
+    }
+
+    diceHTMLString += "</tr>"
+
+    document.getElementById("diceDisplay").innerHTML = diceHTMLString;
+}
+
+//if(6 % 6 == 0){console.log("correct")};
+//if(7 % 6 == 1){console.log("correct")};
+
+
+/* this is the function called when the roll button gets pressed. It finds all the dice inputs and splits them all into two arrays, diceTypes, and dyceCounts. It pases these
+two arrays to the Roll() function and the returned object is stored in a variable which is used to create the dice elements, and update the total dice count text */
+function HandleRoll() {
+    diceInputs = document.getElementById("allDice").length;
+    const diceTypes = [];
+    const diceCounts = [];
+
+    for(let i = 0; i < diceInputs; i++){
+        diceTypes.push(document.getElementById("diceType" + i).selectedOptions[0].value);
+        diceCounts.push(document.getElementById("diceCount" + i).value);
+    }
+
+    const diceRolls = Roll(diceTypes, diceCounts);
+    UpdateTotalText(diceRolls.total);
+
+    AddDiceElements(diceRolls.diceTypes, diceRolls.diceCounts);
+
+    for(let i = 1; i < diceInputs; i++){
+        RemoveDiceSectionFromUi();
+    }
+
+}
+
+//updates the total dice count text with the passed value
+function UpdateTotalText(total) {
+    document.getElementById("totalCount").innerHTML = total;
 }
 
 /* takes two arrays of numbers, one with dice types and one with how many of those types of dice, 
