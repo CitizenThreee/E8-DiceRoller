0d01fcb9b85037c6ae88f1384eca3fd2129da1ff
CitizenThreee <albeisly@gmail.com>
2024-05-02 22:49:21 +1200

    Integrated, tested, and polished the web page

--- diceLogic.js
+++ diceLogic.js
@@ -1,22 +1,25 @@
+
 //creates a new dice input element after the last child of the parent dice input div element
 function AddDiceSectionToUi() {
-    diceInputs = document.getElementById("allDice").length;
+    diceInputCount = document.getElementById("allDice").childElementCount;
+
+    let diceTypeString = diceInputCount == 0 ? "diceType" : "diceType" + diceInputCount;
+    let diceCountString = diceInputCount == 0 ? "diceCount" : "diceCount" + diceInputCount;
 
     document.getElementById("allDice").insertAdjacentHTML("beforeend", 
     `<div id='diceContainer'>
-        <label for='${"diceType" + diceInputs}' id='diceTypeLabel'>Dice Type:</label>
-        <select name='${"diceType" + diceInputs}' id='${"diceType" + diceInputs}'>
-            <option value='D4'>D4</option>
-            <option value='D6'>D6</option>
-            <option value='D8'>D8</option>
-            <option value='D10'>D10</option>
-            <option value='D12'>D12</option>
-            <option value='D20'>D20</option>
+        <label for='${diceTypeString}'>Dice Type:</label>
+        <select name='${diceTypeString}' id='${diceTypeString}'>
+            <option value='4'>D4</option>
+            <option value='6' selected>D6</option>
+            <option value='8'>D8</option>
+            <option value='10'>D10</option>
+            <option value='12'>D12</option>
+            <option value='20'>D20</option>
         </select>
-        <label for='${"diceCount" + diceInputs}' id='diceCountLabel'>Dice Count:</label>
-        <input type='number' value='0' id='${"diceCount" + diceInputs}' name='${"diceCount" + diceInputs}'>
-    </div>`
-);
+        <label for='${diceCountString}'>Dice Count:</label>
+        <input type='number' value='0' id='${diceCountString}' name='${diceCountString}'>
+    </div>`,);
 }
 
 //Removes the last element child from the dice input div element
@@ -28,21 +31,23 @@ function RemoveDiceSectionFromUi() {
 function AddDiceElements(diceTypes, diceCounts){
     let diceHTMLString = "<tr>";
 
-    for(let i = 0; i < diceTypes.length; i++){
-        if(i % 6 == 1){diceHTMLString += "<tr>"}
+    for(let i = 1; i < diceTypes.length + 1; i++){
+        if(i % 6 == 1 && i != 1){diceHTMLString += "<tr>"}
 
         diceHTMLString += 
         `<td>
-            <p class="diceHeader">${diceTypes[i]}</p>
-            <button class="diceButton">${diceCounts[i]}</button>
+            <p class="diceHeader">${diceTypes[i - 1]}</p>
+            <button class="diceButton">${diceCounts[i - 1]}</button>
         </td>`
 
-        if(i % 6 == 0 || i == diceTypes.length - 1){diceHTMLString += "</tr>"}
+        if(i != 0 && i % 6 == 0 || i == diceTypes.length){diceHTMLString += "</tr>"}
     }
 
     diceHTMLString += "</tr>"
 
     document.getElementById("diceDisplay").innerHTML = diceHTMLString;
+
+    console.log(diceHTMLString);
 }
 
 //if(6 % 6 == 0){console.log("correct")};
@@ -52,24 +57,22 @@ function AddDiceElements(diceTypes, diceCounts){
 /* this is the function called when the roll button gets pressed. It finds all the dice inputs and splits them all into two arrays, diceTypes, and dyceCounts. It pases these
 two arrays to the Roll() function and the returned object is stored in a variable which is used to create the dice elements, and update the total dice count text */
 function HandleRoll() {
-    diceInputs = document.getElementById("allDice").length;
+    diceInputCount = document.getElementById("allDice").childElementCount;
+    if(diceInputCount <= 0) {return};
     const diceTypes = [];
     const diceCounts = [];
 
-    for(let i = 0; i < diceInputs; i++){
-        diceTypes.push(document.getElementById("diceType" + i).selectedOptions[0].value);
-        diceCounts.push(document.getElementById("diceCount" + i).value);
+    for(let i = 0; i < diceInputCount; i++){
+        let diceTypeString = i == 0 ? "diceType" : "diceType" + i;
+        let diceCountString = i == 0 ? "diceCount" : "diceCount" + i;
+        diceTypes.push(document.getElementById(diceTypeString).selectedOptions[0].value);
+        diceCounts.push(document.getElementById(diceCountString).value);
     }
 
     const diceRolls = Roll(diceTypes, diceCounts);
     UpdateTotalText(diceRolls.total);
 
     AddDiceElements(diceRolls.diceTypes, diceRolls.diceCounts);
-
-    for(let i = 1; i < diceInputs; i++){
-        RemoveDiceSectionFromUi();
-    }
-
 }
 
 //updates the total dice count text with the passed value
@@ -121,6 +124,13 @@ function Roll(diceTypes, diceCounts){
         "total" : diceTotal
     }
 }
+
+//Check correctness of the randomness function ( within 0.1 of the expected median(3.5) every time)
+/* let sumOfRandom = 0;
+for(let i = 0; i < 1000; i++){
+    sumOfRandom += GetRandomInt(1, 6);
+}
+console.log(sumOfRandom / 1000); */
 
 function GetRandomInt(min, max) { // min and max included
     min = Number(min);
